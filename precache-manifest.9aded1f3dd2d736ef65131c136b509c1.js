3b0e817cdf85df8605366a90335dfc947e72bf5a
Purevdorj Mungunreis <purevdorj.m@tt.local>
2020-11-10 10:33:24 +0800

    Updates

---
+++ precache-manifest.9aded1f3dd2d736ef65131c136b509c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80487903138a19862227fef5fe989bb6",
+    "url": "/burgerReact/index.html"
+  },
+  {
+    "revision": "f4db6db64fb335fd941c",
+    "url": "/burgerReact/static/css/main.e21c870c.chunk.css"
+  },
+  {
+    "revision": "35a5562441601c74f115",
+    "url": "/burgerReact/static/js/2.bc1b0639.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burgerReact/static/js/2.bc1b0639.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4db6db64fb335fd941c",
+    "url": "/burgerReact/static/js/main.dda5fd09.chunk.js"
+  },
+  {
+    "revision": "41d82c1af717eff87aab",
+    "url": "/burgerReact/static/js/runtime-main.f483bba5.js"
+  },
+  {
+    "revision": "332a70d3228c88d9586c2f952d32d47b",
+    "url": "/burgerReact/static/media/burger-logo.332a70d3.png"
+  }
+]);
